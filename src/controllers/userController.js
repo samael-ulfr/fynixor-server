48e256ab429d635a0714922b2315750271d9baba
Chinmay <ckvpatil20@gmail.com>
2025-09-08 16:19:27 +0530

    same site none logout

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -119,7 +119,7 @@ exports.logout = (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
   });
   return res.status(200).json({ message: "Logout successful" });
 };
