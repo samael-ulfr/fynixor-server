e5b8212569e20c952c11f556feb460036a4ecc28
Chinmay <ckvpatil20@gmail.com>
2025-09-08 15:01:32 +0530

    add cors 1

--- src/server.js
+++ src/server.js
@@ -15,10 +15,19 @@ const app = express();
 
 // Connect to MongoDB
 connectDB();
-
+const allowedOrigins = ["http://localhost:5173", "http://example.com"];
+ 
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend URL
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true, // allow cookies
   })
 );
